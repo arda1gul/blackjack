ff3d7f8748a88a95bd5f2523f1a5d9b83fe27661
arda1gul <112638451+arda1gul@users.noreply.github.com>
2023-07-15 21:55:17 +0300

    gestart

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ console.log('BlackJack Loaded');
 let balance = document.querySelector('.saldo');
 const submit = document.querySelector('.zet-in');
 
-submit.addEventListener('click', enterFunction());
 
-function enterFunction(event) {
-    if(event.keycode == 'Enter') {
-        submitFunction()
-    } else {
-        alert('Function niet gelukt')
+function play() {
+    let inzet = document.querySelector('.bet');
+    let balanceChips = 100000;
+    let newBalance = balance - inzet.value
+    if(inzet.value > 0 && inzet.value < balance) {
+        
     }
-} 
+}
